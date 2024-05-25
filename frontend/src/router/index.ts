c0f42b58d35a8a2fca694d1afcca18e87ccf5516
z1ck <jose.foitzick@usach.cl>
2024-05-25 18:03:06 -0400

    Autenticacion JWT por vue router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,17 +4,27 @@ import VolunteersPage from '../pages/VolunteersPage.vue';
 import VolunteersInRadiusPage from '../pages/VolunteersInRadiusPage.vue';
 import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
-
-
+import { store } from '../store';
 
 const routes: RouteRecordRaw[] = [
   { path: '/', component: Login },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
-  { path: '/emergencies', component: EmergenciesPage },
-  { path: '/volunteers', component: VolunteersPage },
-  { path: '/volunteersInRadius', component: VolunteersInRadiusPage },
-  
+  { 
+    path: '/emergencies', 
+    component: EmergenciesPage,
+    meta: { requiresAuth: true }
+  },
+  { 
+    path: '/volunteers', 
+    component: VolunteersPage,
+    meta: { requiresAuth: true }
+  },
+  { 
+    path: '/volunteersInRadius', 
+    component: VolunteersInRadiusPage,
+    meta: { requiresAuth: true }
+  },
 ];
 
 const router = createRouter({
@@ -22,5 +32,17 @@ const router = createRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.token) {
+      alert('Por favor, inicie sesión.');
+      next('/Login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router;
